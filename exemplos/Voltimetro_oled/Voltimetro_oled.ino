6624df269e4b64e6ea1945c8a69a7c59a20f5743
PauloAlvarenga <32138529+PauloAlvarenga@users.noreply.github.com>
2017-10-29 20:36:05 -0200

    arq

---
+++ exemplos/Voltimetro_oled/Voltimetro_oled.ino
@@ -0,0 +1,78 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x64 size display using SPI to communicate
+4 or 5 pins are required to interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// If using software SPI (the default case):
+#define OLED_MOSI   23
+#define OLED_CLK   18
+#define OLED_DC    16
+#define OLED_CS    17
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+/* Uncomment this block to use hardware SPI
+#define OLED_DC     6
+#define OLED_CS     7
+#define OLED_RESET  8
+Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
+*/
+
+#if (SSD1306_LCDHEIGHT != 64)
+//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {                
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(200);
+
+}
+
+
+void loop() {
+
+  int sensorValue = analogRead(34);
+  float voltage = sensorValue * (3.16 / 4096.0);
+    
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0,00);
+  display.clearDisplay();
+  display.print(voltage);
+  display.print("V");
+  display.display();
+  delay(500);
+
+    
+  Serial.println(voltage);
+  Serial.println(sensorValue);
+}
+
+
